dbc07b1c3845d35bbe4b9c64870ad14655148abc
victoremmanuel8 <victor.emmanuel.lima493@gmail.com>
2024-01-29 23:53:10 -0300

    1

--- HANDS/platforms/android/app/src/main/assets/www/js/index.js
+++ HANDS/platforms/android/app/src/main/assets/www/js/index.js
@@ -27,3 +27,4 @@ function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
+
