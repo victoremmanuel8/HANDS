2b0f314d3e365d61a4620f79eb10b1a633a30bad
victoremmanuel8 <victor.emmanuel.lima493@gmail.com>
2024-03-01 00:59:17 -0300

    01.03.2024

--- HANDS/platforms/android/app/src/main/assets/www/js/ver-mais.js
+++ HANDS/platforms/android/app/src/main/assets/www/js/ver-mais.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
   var maisConteudo = document.querySelector('.mais-conteudo');
 
   lerMaisBtn.addEventListener('click', function() {
-    if (maisConteudo.style.display === 'none') {
-      maisConteudo.style.display = 'block';
-      lerMaisBtn.innerText = 'ver menos';
-    } else {
-      maisConteudo.style.display = 'none';
-      lerMaisBtn.innerText = 'Ver mais...';
-    }
+      if (maisConteudo.style.display === 'none' || maisConteudo.style.display === '') {
+          maisConteudo.style.display = 'block';
+          lerMaisBtn.innerText = 'ver menos';
+      } else {
+          maisConteudo.style.display = 'none';
+          lerMaisBtn.innerText = 'Ver mais...';
+      }
   });
 });
