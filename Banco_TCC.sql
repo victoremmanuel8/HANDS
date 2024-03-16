27826194538c37ef56003e35787071165034b98f
Gabriel Logan Sanches <gabriellogan2006@gmail.com>
2024-03-16 20:58:57 -0300

    Banco + Sequelize atv Ivan
    População do banco, leve alteração, todos select query pendentes foram feitos.

--- Banco_TCC.sql
+++ Banco_TCC.sql
@@ -5,7 +5,6 @@ use hands_db;
 
 #registro de cadastro
 create table tb_usuario (
-
 id_usuario INT primary key auto_increment,
 nm_nome VARCHAR (100) not null,
 nm_sobrenome VARCHAR (100) not null,
@@ -17,13 +16,12 @@ dt_nascimento DATE
 
 INSERT INTO tb_usuario (nm_nome, nm_sobrenome, email, senha, dt_nascimento)
 	VALUES('Luan', 'Henrique', 'luanhenrique123@gmail.com', 'luan123', '2004-06-06'),
-		  ('Gabriel Logan', 'Sanches', 'gabriel.sanches31@etec.sp.gov.br', 'senha', '2006-06-15'),
-		  ('Pyetra', 'Quintiana', 'pyetra.quintiana@etec.sp.gov.br', 'amosapos', '2007-04-13'),
-          ('Maria', 'Santos',  'maria.santos2815@etec.sp.gov.br', 'rock4life', '2004-01-25'),
-          ('Victor', 'Silva', 'victor.silva974@etec.sp.gov.br', 'amoaR', '2005-01-29');
+                  ('Gabriel Logan', 'Sanches', 'gabriel.sanches31@etec.sp.gov.br', 'senha', '2006-06-15'),
+                  ('Pyetra', 'Quintiana', 'pyetra.quintiana@etec.sp.gov.br', 'amosapos', '2007-04-13'),
+                  ('Maria', 'Santos',  'maria.santos2815@etec.sp.gov.br', 'rock4life', '2004-01-25'),
+                  ('Victor', 'Silva', 'victor.silva974@etec.sp.gov.br', 'amoaR', '2005-01-29');
           
 create table tb_profissional (
-
 id_profissional INT primary key auto_increment,
 nm_prof VARCHAR (100) not null,
 nm_sobrenome VARCHAR (100) not null,
@@ -32,6 +30,15 @@ email VARCHAR(100) not null UNIQUE,
 senha VARCHAR(255) not null,
 dt_nascimento DATE
 );
+
+INSERT INTO tb_profissional (nm_prof, nm_sobrenome, cd_rg, email, senha, dt_nascimento)
+    VALUES('Aline', 'Caruso', '12.345.678-9', 'aline.caruso1@hotmail.com', 'amodaraula', '1988-10-04'),
+                  ('Fernando', 'Costa', '75.284.912-0', 'fernandocostaprofessor@hotmail.com', 'vaiporco', '1965-03-25'),
+                  ('Ingrid', 'Souza', '98.765.143-2', 'ingridsouzaprof1213@gmail.com', 'belinha2304', '1999-12-13'),
+                  ('Marcos', 'Alves', '24.681.357-9', 'profmarcosalves84@gmail.com', 'mjf0784', '1984-10-04'),
+                  ('Henrique', 'Santos', '11.223.344-5', 'professorhenriquesantos2000@gmail.com', 'mvsgnts1754', '2000-05-17'),
+                  ('Anderson', 'Silva', '55.555.555-5', 'professorandersonsilva94@gmail.com', 'coringaominhavida', '1994-01-04'),
+                  ('Paola', 'Arantes', '98.765.432-0', 'profpaola0204@gmail.com', 'pnatugj38', '1990-02-04');
 
 CREATE TABLE tb_categoria (
     id_categoria INT PRIMARY KEY AUTO_INCREMENT,
@@ -96,6 +103,13 @@ CREATE TABLE tb_premium (
     FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
 );
 
+INSERT INTO tb_premium (id_usuario, dt_inicio, dt_fim, status)
+    VALUES  (1, '2024-01-01', '2025-01-01', 'ativo'),
+                    (2, '2024-01-01', '2025-02-01', 'ativo'),
+                    (3, '2024-01-01', '2025-03-01', 'ativo'),
+                    (4, '2024-01-01', '2025-01-01', 'cancelado'),
+                    (5, '2024-01-01', '2025-02-01', 'cancelado');
+
 /*
 CREATE TABLE tb_videos
 CREATE TABLE tb_aulas_premium
@@ -105,4 +119,3 @@ CREATE tb_video_premium */
 select * from tb_usuario;
 
 select * from tb_profissional;
-
--- Querys/QuerySelects.js
+++ Querys/QuerySelects.js
@@ -25,7 +25,11 @@ async function runServer() {
 
     console.log(resultBusca)
 
-
+  /*
+    -- /// [Query Original 02]: ///
+            
+    select nm_nome, dt_nascimento from tb_usuario order by dt_nascimento desc
+    */
 
 
 resultBusca = await tb_usuario.findAll({
@@ -35,6 +39,136 @@ resultBusca = await tb_usuario.findAll({
     ],
     order:[
         Sequelize.literal('dt_nascimento DESC')
+    ],
+    raw:true
+  })
+
+console.log(resultBusca)
+
+/*
+  -- /// [Query Original 03]: ///
+
+  select status, COUNT(*) from tb_premium group by status
+*/
+
+resultBusca = await tb_premium.findAll({
+    where: {
+        status:{
+            [Op.in]:[
+                'ativo',
+                'cancelado'
+            ]
+        }
+    },
+    attributes:[
+        'status',
+        [Sequelize.fn('COUNT', Sequelize.col('*')), 'qtd']
+    ],
+    group:[
+        'status'
+    ],
+    raw:true
+  })
+
+console.log(resultBusca)
+
+/*
+  -- /// [Query Original 04]: ///
+
+  select id_assinatura, status, COUNT(*) 'qtd' from tb_premium group by id_assinatura, status order by id_assinatura desc
+*/
+
+resultBusca = await tb_premium.findAll({
+    where: {
+        status:{
+            [Op.in]:[
+                'ativo',
+                'cancelado'
+            ]
+        }
+    },
+    attributes:[
+        'id_assinatura',
+        'status',
+        [Sequelize.fn('COUNT', Sequelize.col('*')), 'qtd']
+    ],
+    order:[
+        Sequelize.literal('id_assinatura DESC')
+    ],
+    group:[
+        'id_assinatura',
+        'status'
+    ],
+    raw:true
+  })
+
+console.log(resultBusca)
+
+/*
+  -- /// [Query Original 05]: ///
+
+  select  nm_nome, status from tb_premium inner join tb_usuario on (tb_usuario.id_usuario = tb_premium.id_usuario)
+*/
+
+resultBusca = await tb_premium.findAll({
+    include:{
+        model: tb_usuario,
+        required: true,
+        attributes: ['nm_nome'],
+    },
+    attributes:[
+        'status',
+    ],
+    raw:true
+  })
+
+console.log(resultBusca)
+
+/*
+  -- /// [Query Original 06]: ///
+
+    select tb_aula.titulo, tb_profissional.id_profissional, tb_profissional.nm_prof, tb_profissional.nm_sobrenome, tb_categoria.nm_categoria from tb_aula inner join tb_profissional on tb_aula.tb_profissional_id = tb_profissional.id inner join  tb_categoria on tb_aula.tb_categoria_id = tb_categoria.id
+*/
+
+resultBusca = await tb_aula.findAll({
+    include:[{
+        model: tb_profissional,
+        required: true,
+        attributes: ['id_profissional', 'nm_prof', 'nm_sobrenome'],
+    }, {
+        model: tb_categoria,
+        required: true,
+        attributes:['nm_categoria'],
+    }],
+    attributes:[
+        'titulo'
+    ],
+    raw:true
+  })
+
+console.log(resultBusca)
+
+/*
+  -- /// [Query Original 07]: ///
+
+  select  id_usuario, status, count(*) 'qtd' from tb_premium inner join tb_usuario on (tb_usuario.id_usuario = tb_premium.id_usuario) group by id_usuario, status order by id_usuario desc
+*/
+
+resultBusca = await tb_premium.findAll({
+    include:{
+        model: tb_usuario,
+        required: true,
+        attributes: ['id_usuario'],
+    },
+    attributes:[
+        'status',
+        [Sequelize.fn('COUNT', Sequelize.col('*')), 'qtd']
+    ],
+    group:[
+        'status'
+    ],
+    order:[
+        Sequelize.literal('id_usuario DESC')
     ],
     raw:true
   })
