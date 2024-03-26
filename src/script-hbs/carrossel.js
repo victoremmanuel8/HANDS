fad952a0a5310bd69d411b66d96f0e5fa043aa1f
victoremmanuel8 <victor.emmanuel.lima493@gmail.com>
2024-03-26 04:21:45 -0300

    26.03.2024

--- src/script-hbs/carrossel.js
+++ src/script-hbs/carrossel.js
@@ -1,4 +1,47 @@
 
+// const carousel = document.querySelector(".carousel");
+// let isDragging = false,
+//     startPosition,
+//     startScrollLeft;
+
+// carousel.addEventListener("mousedown", (e) => {
+//     isDragging = true;
+//     startPosition = e.pageX || e.touches[0].pageX;
+//     startScrollLeft = carousel.scrollLeft;
+//     carousel.style.scrollBehavior = "auto"; // Desativa o comportamento de rolagem suave temporariamente
+// });
+
+// carousel.addEventListener("mouseup", () => {
+//     isDragging = false;
+//     carousel.style.scrollBehavior = "smooth"; // Reativa o comportamento de rolagem suave
+// });
+
+// carousel.addEventListener("mousemove", (e) => {
+//     if (!isDragging) return;
+//     const currentPosition = e.pageX || e.touches[0].pageX;
+//     const distance = currentPosition - startPosition;
+//     carousel.scrollLeft = startScrollLeft - distance;
+// });
+
+// carousel.addEventListener("touchstart", (e) => {
+//     isDragging = true;
+//     startPosition = e.touches[0].pageX;
+//     startScrollLeft = carousel.scrollLeft;
+//     carousel.style.scrollBehavior = "auto"; // Desativa o comportamento de rolagem suave temporariamente
+// });
+
+// carousel.addEventListener("touchend", () => {
+//     isDragging = false;
+//     carousel.style.scrollBehavior = "smooth"; // Reativa o comportamento de rolagem suave
+// });
+
+// carousel.addEventListener("touchmove", (e) => {
+//     if (!isDragging) return;
+//     const currentPosition = e.touches[0].pageX;
+//     const distance = currentPosition - startPosition;
+//     carousel.scrollLeft = startScrollLeft - distance;
+// });
+
 const carousel = document.querySelector(".carousel");
 let isDragging = false,
     startPosition,
@@ -6,19 +49,19 @@ let isDragging = false,
 
 carousel.addEventListener("mousedown", (e) => {
     isDragging = true;
-    startPosition = e.pageX || e.touches[0].pageX;
+    startPosition = e.pageX;
     startScrollLeft = carousel.scrollLeft;
-    carousel.style.scrollBehavior = "auto"; // Desativa o comportamento de rolagem suave temporariamente
+    carousel.style.scrollBehavior = "auto";
 });
 
 carousel.addEventListener("mouseup", () => {
     isDragging = false;
-    carousel.style.scrollBehavior = "smooth"; // Reativa o comportamento de rolagem suave
+    carousel.style.scrollBehavior = "smooth";
 });
 
 carousel.addEventListener("mousemove", (e) => {
     if (!isDragging) return;
-    const currentPosition = e.pageX || e.touches[0].pageX;
+    const currentPosition = e.pageX;
     const distance = currentPosition - startPosition;
     carousel.scrollLeft = startScrollLeft - distance;
 });
@@ -27,12 +70,12 @@ carousel.addEventListener("touchstart", (e) => {
     isDragging = true;
     startPosition = e.touches[0].pageX;
     startScrollLeft = carousel.scrollLeft;
-    carousel.style.scrollBehavior = "auto"; // Desativa o comportamento de rolagem suave temporariamente
+    carousel.style.scrollBehavior = "auto";
 });
 
 carousel.addEventListener("touchend", () => {
     isDragging = false;
-    carousel.style.scrollBehavior = "smooth"; // Reativa o comportamento de rolagem suave
+    carousel.style.scrollBehavior = "smooth";
 });
 
 carousel.addEventListener("touchmove", (e) => {
@@ -40,4 +83,10 @@ carousel.addEventListener("touchmove", (e) => {
     const currentPosition = e.touches[0].pageX;
     const distance = currentPosition - startPosition;
     carousel.scrollLeft = startScrollLeft - distance;
+});
+
+carousel.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    const scrollAmount = e.deltaY * 5; // Ajuste a sensibilidade de rolagem conforme necessário
+    carousel.scrollLeft += scrollAmount;
 });
