ece65f8e9cfa8ff97062532496118f40b4998ca6
victoremmanuel8 <128409805+victoremmanuel8@users.noreply.github.com>
2024-04-04 16:02:33 -0300

    AAAA

--- config/bdConnection.js
+++ config/bdConnection.js
@@ -1,9 +1,9 @@
  const {Sequelize} = require ('sequelize')
- const usuario = 'root'
+ const usuario =  'root'
  const senha = 'root'
  const nmDB = 'hands_db'
  const connSequelize =  new Sequelize(
-     `mysql:${usuario}:${senha}@localhost:3307/${nmDB}` //porta 3307(escola) ou 3306 (casa)
+    `mysql:${usuario}:${senha}@mysql.railway.internal:3306/${nmDB}`//porta 3307(escola) ou 3306 (casa)z
  )
 
  module.exports = {
