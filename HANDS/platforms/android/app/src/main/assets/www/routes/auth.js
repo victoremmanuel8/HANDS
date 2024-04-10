ba588a6e56c60775a42a7def53e53f0946b3a93d
LuanHenrique51 <luanhenriquesp159@gmail.com>
2024-04-09 22:17:27 -0300

    09.04.2024

---
+++ HANDS/platforms/android/app/src/main/assets/www/routes/auth.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const req = require("../../node_modules/req/node_modules/mime");
+const router = express.Router();
+const authControllerregister = require('../controllers/authregister');
+const authControllerlogin = require('../controllers/authlogin');
+const authControllerprof = require('../controllers/authregister_prof');
+const authControllerlogprof = require('../controllers/authlogin_prof');
+
+
+//aqui a função get e render vai pegar a url e renderizar ela no site
+            'auth/cadastro'
+router.post("/cadastro", authControllerregister.register) //teste
+
+                'auth/cadastro_prof'
+router.post("/cadastro_prof", authControllerprof.register)
+
+'authlogin/login_prof'
+router.post("/login_prof", authControllerlogprof.login)
+
+
+
+//validações do login
+                'auth/login'
+router.post("/login", (req, res) => {
+    const { email, senha } = req.body; 
+  
+    const erros = [];
+  
+    if (!email || typeof email === 'undefined' || email === null) {
+        erros.push({ text: "Email Inválido" });
+    }
+  
+    if (!senha || typeof senha === 'undefined' || senha === null) {
+        erros.push({ text: "Senha inválida" });
+    } else if (senha.length < 3) {
+        erros.push({ text: "Senha da categoria muito pequena" });
+    }
+  
+    if (erros.length > 0) {
+        res.render("login", { erros: erros });
+    } else {
+        authControllerlogin.login(req, res);
+    }
+  });
+
+
+module.exports = router;
