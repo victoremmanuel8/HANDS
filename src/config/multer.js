7f33a345861c309625572ddc7ba1221239fad70f
LuanHenrique51 <luanhenriquesp159@gmail.com>
2024-04-12 15:34:43 -0300

    12.04.2024
    Finalizando o back para armazenamento de videos e fotos tanto local como na nuve(AWS)

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,23 +1,50 @@
 const multer = require('multer');
 const path = require('path');
-const crypto = require('crypto')
+const crypto = require('crypto');
+const aws = require('aws-sdk')
+const multerS3 = require('multer-s3');
+
+
+const storageTypes = {
+  local: multer.diskStorage({
+      destination: (req, file, cb) => {
+          cb(null, path.resolve(__dirname, '..', '..','tmp', 'uploads'));
+},
+filename: (req, file, cb) => {
+  crypto.randomBytes(16, (err, hash) => {
+    if (err) cb(err);
+
+     file.key = `${hash.toString('hex')}-${file.originalname}`;
+
+    cb(null, file.key)
+      });
+    }
+  }),
+  s3: multerS3({
+      s3: new aws.S3(),
+      bucket: 'uploadexample2',
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      acl: 'public-read', 
+      key: (req, file, cb) => {
+        crypto.randomBytes(16, (err, hash) => {
+          if (err) cb(err);
+      
+          const fileName = `${hash.toString('hex')}-${file.originalname}`;
+      
+          cb(null, fileName)
+            });
+      },
+  }),
+}
+
+/*const storageS3 = new aws.S3({
+  accessKeyId: '',
+  secretAccessKey,
+})*/
 
 module.exports = {
   dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, path.resolve(__dirname, '..', '..','tmp', 'uploads'));
-    },
-    filename: (req, file, cb) => {
-      crypto.randomBytes(16, (err, hash) => {
-        if (err) cb(err);
-
-        const fileName = `${hash.toString('hex')}-${file.originalname}`;
-
-        cb(null, fileName)
-      });
-    },
-  }),
+  storage: storageTypes['local'], //s3 quando tiver na nuvem da amazon
   limits: {
     fileSize: 100 * 1024 * 1024,
   },
