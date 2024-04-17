2a9c20749edf5fc44d2bc1e45d4b92ed7e58c3dd
Marife1 <137432496+Marife1@users.noreply.github.com>
2024-04-17 17:56:29 -0300

    17/04/2024
    fazendo o login com o google usando passport

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -77,3 +77,16 @@ app.post('cadastro', (req, res) => {
       res.status(200).send('Usuário cadastrado com sucesso');
   });
 });*/
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID:     process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/auth/google/callback",
+    passReqToCallback   : true
+},
+function(request, accessToken, refreshToken, profile, done) {
+    done(err, user);
+}
+));
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,12 +1,48 @@
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
 const path = require('path');
 const app = express();
+require('./auth');
 
 app.use(express.json());
 app.use (express.static(path.join(__dirname, '../client')));
 
+function isLoggendIn(req, res, next) {
+    req.user ? next() : res.sendStatus(401);
+}
+
 app.get ('/', (req, res) => {
     res.sendFile ('index.html');
+});
+
+// app.use(session({
+//     secret: 'mysecret',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: false }
+// }))
+
+// app.use (passport.initialize());
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+}));
+
+app.get('/auth/google/failure', (res, req) => {
+    res.send('Something went wrong!');
+});
+
+app.get('/auth/protected', isLoggendIn, (res, req) => {
+    let name = req.user.displayName;
+    res.send(`Hello ${nome}`);
 });
 
 app.listen (5000, () => {
