45c3930157b4b2f30cb70f4063111839a9b1c1d0
victoremmanuel8 <128409805+victoremmanuel8@users.noreply.github.com>
2024-04-23 14:28:51 -0300

    23.04.2024

--- src/config/bdConnection.js
+++ src/config/bdConnection.js
@@ -3,7 +3,7 @@
  const senha = process.env.DATABASE_PASSWORD
  const nmDB = process.env.DATABASE
  const connSequelize =  new Sequelize(
-     `mysql:${usuario}:${senha}@localhost:3306/${nmDB}` //porta 3307(escola) ou 3306 (casa)
+     `mysql:${usuario}:${senha}@localhost:3307/${nmDB}` //porta 3307(escola) ou 3306 (casa)
  )
 
  module.exports = {
