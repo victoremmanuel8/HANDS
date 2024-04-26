72c63d03b5f7108de65e5e63d5a71145852caf6d
victoremmanuel8 <victor.emmanuel.lima493@gmail.com>
2024-04-26 15:56:09 -0300

    26.04.2024

---
+++ src/script/lisLogin.js
@@ -0,0 +1,27 @@
+function escreverTexto(texto, elemento, velocidade = 20, callback) {
+    let i = 0;
+    const intervalo = setInterval(function() {
+        elemento.textContent += texto.charAt(i);
+        i++;
+        if (i > texto.length) {
+            clearInterval(intervalo);
+            callback();
+        }
+    }, velocidade);
+}
+
+const textos = [
+    "Olá, novamente 🤩🥰"
+];
+
+const elementos = document.getElementById('texto1');
+
+function exibirTexto(index) {
+    if (index < textos.length) {
+        escreverTexto(textos[index], elementos[index], 100, () => {
+            exibirTexto(index + 1);
+        });
+    }
+}
+
+exibirTexto(0);
