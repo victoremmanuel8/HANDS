087b8e5bb8f2d9c5896d60531a62a233ebd4f51b
Marife1 <137432496+Marife1@users.noreply.github.com>
2024-04-30 17:42:50 -0300

    30/04/2024

--- src/config/bdConnection.js
+++ src/config/bdConnection.js
@@ -1,25 +1,25 @@
- const {Sequelize} = require ('sequelize')
- const usuario = process.env.DATABASE_USER
- const senha = process.env.DATABASE_PASSWORD
- const nmDB = process.env.DATABASE
- const connSequelize =  new Sequelize(
-     `mysql:${usuario}:${senha}@roundhouse.proxy.rlwy.net:26139/${nmDB}` //porta 3307(escola) ou 3306 (casa)
- )
+//  const {Sequelize} = require ('sequelize')
+//  const usuario = process.env.DATABASE_USER
+//  const senha = process.env.DATABASE_PASSWORD
+//  const nmDB = process.env.DATABASE
+//  const connSequelize =  new Sequelize(
+//      `mysql:${usuario}:${senha}@roundhouse.proxy.rlwy.net:26139/${nmDB}` //porta 3307(escola) ou 3306 (casa)
+//  )
 
- module.exports = {
-     connSequelize,
-     nmDB
- }
+//  module.exports = {
+//      connSequelize,
+//      nmDB
+//  }
 
-// const {Sequelize} = require ('sequelize')
-// const usuario = 'root'
-// const senha = 'root'
-// const nmDB = 'hands_db'
-// const connSequelize =  new Sequelize(
-//     `mysql:${usuario}:${senha}@localhost:3306/${nmDB}` //porta 3307(escola) ou 3306 (casa)
-// )
+const {Sequelize} = require ('sequelize')
+const usuario = 'root'
+const senha = 'root'
+const nmDB = 'hands_db'
+const connSequelize =  new Sequelize(
+    `mysql:${usuario}:${senha}@localhost:3307/${nmDB}` //porta 3307(escola) ou 3306 (casa)
+)
 
-// module.exports = {
-//     connSequelize,
-//     nmDB
-// }
+module.exports = {
+    connSequelize,
+    nmDB
+}
